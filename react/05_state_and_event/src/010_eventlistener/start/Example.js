19658f0bc2d359e441b971aa250e2dbccb4fe423
Yuta Okuma <y.dream.nest@icloud.com>
2022-10-17 08:58:45 +0900

    feat: eventListener/execute functions with events

--- react/05_state_and_event/src/010_eventlistener/start/Example.js
+++ react/05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,20 @@
 const Example = () => {
+  const clickHander = () => {
+    alert("ボタンがクリックされました。");
+  };
+
+  const clickHander2 = () => {
+    console.log("ボタンがクリックされました。");
+  };
+
+  // ()をつけると実行結果が返ってくる（return文がないのでundefined）
+  // console.log(clickHander());
+
   return (
-    <></>
+    <>
+      <button onClick={clickHander}>クリックしてね</button>
+      <button onClick={clickHander2}>クリックしてね</button>
+    </>
   );
 };
 
