f9300ffd48bbf9be863fb38df2be4c27cf6da26d
Yuta Okuma <y.dream.nest@icloud.com>
2022-10-23 22:13:18 +0900

    feat: conditionalRender/conditional-render

--- react/06_control_and_form/src/060_conditional_render/start/Example.js
+++ react/06_control_and_form/src/060_conditional_render/start/Example.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
+// if文、&&、??（Null合体演算子）、三項演算子
+// A ?? B
+// A null undefined
+
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +19,27 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+            // if (animal === "Dog") {
+            //   return <li key={animal}>{animal}★</li>;
+            // } else {
+            //   return <li key={animal}>{animal}</li>;
+            // }
+            // 三項演算子
+            // return (
+            //   <li key={animal}>{animal === "Dog" ? animal + "★" : animal}</li>
+            // );
+            return (
+              <li key={animal}>
+                {animal ?? "null,undefinedでした"}
+                {animal === "Dog" && "★"}
+              </li>
+            );
+          })}
       </ul>
     </>
   );
