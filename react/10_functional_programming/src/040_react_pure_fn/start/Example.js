baf6f05d9ca23bddb8f740e22e989cace3c05376
Yuta Okuma <y.dream.nest@icloud.com>
2022-11-15 08:13:04 +0900

    feat: functional-programming/react & pure function

--- react/10_functional_programming/src/040_react_pure_fn/start/Example.js
+++ react/10_functional_programming/src/040_react_pure_fn/start/Example.js
@@ -1,7 +1,30 @@
+// Reactと純粋関数
+let value = 0;
+
+const Child = () => {
+  value++;
+  return <div>{value}</div>;
+};
+
+// 解決策として引数で渡すようにする
+const ChildPure = ({ value }) => {
+  return <div>{value}</div>;
+};
+
+// 引数を受け取ってJSXを返す純粋関数
 const Example = () => {
+  let value = 0;
 
   return (
     <>
+      {/* 関数スコープ外の変数は使用してはならない */}
+      {/*<div>{value}</div>*/}
+      <Child />
+      <Child />
+      <Child />
+      <ChildPure value={++value} />
+      <ChildPure value={++value} />
+      <ChildPure value={++value} />
     </>
   );
 };
