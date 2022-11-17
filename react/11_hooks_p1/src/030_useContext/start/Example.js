fce85c014284dc2824d1e5edf39f709f75bb327d
Yuta Okuma <y.dream.nest@icloud.com>
2022-11-17 09:19:11 +0900

    feat: react-hooks/useContext

--- react/11_hooks_p1/src/030_useContext/start/Example.js
+++ react/11_hooks_p1/src/030_useContext/start/Example.js
@@ -1,8 +1,12 @@
+// useContextの基礎
+import { createContext } from "react";
 import Child from "./components/Child";
 
+export const MyContext = createContext("hello");
+
 const Example = () => {
-  const value = 'hello'
-  return <Child value={value}/>;
+  // const value = "hello";
+  return <Child />;
 };
 
 export default Example;
