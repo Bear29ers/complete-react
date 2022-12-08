dd78a1782d0a1d7f1b1c565281f6b5a9cc304c26
Yuta Okuma <y.dream.nest@icloud.com>
2022-12-08 09:19:14 +0900

    feat: redux/use basic redux

--- react/13_redux/start/src/010_redux_no_rtk/Example.js
+++ react/13_redux/start/src/010_redux_no_rtk/Example.js
@@ -1,11 +1,15 @@
+// 素のReduxでグローバルな状態管理を記述する
 import Counter from "./components/Counter";
 import { CounterProvider } from "./context/CounterContext";
+import { Provider } from "react-redux";
+import store from "./store";
 
+// useContext & useReducer
 const Example = () => {
   return (
-    <CounterProvider>
+    <Provider store={store}>
       <Counter />
-    </CounterProvider>
+    </Provider>
   );
 };
 
