883b44dd85255f6a4cadf6fd95beeac729402b04
Yuta Okuma <y.dream.nest@icloud.com>
2022-12-17 19:20:33 +0900

    feat: redux/asynchronous with redux thunk

--- react/13_redux/start/src/050_redux_thunk/store/modules/counter.js
+++ react/13_redux/start/src/050_redux_thunk/store/modules/counter.js
@@ -1,9 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { asyncCount } from "../../api/counter";
 
 const counter = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState: {
-    count: 0
+    count: 0,
   },
   reducers: {
     add(state, { type, payload }) {
@@ -17,11 +18,21 @@ const counter = createSlice({
       // const newState = { ...state };
       // newState.count = state.count - payload
       // return newState;
-    }
-  }
-})
+    },
+  },
+});
 
 const { add, minus } = counter.actions;
 
-export { add, minus }
-export default counter.reducer
+const addAsync = (payload) => {
+  return async (dispatch, getState) => {
+    // const state = getState();
+    // console.log(state);
+    const response = await asyncCount(payload);
+    // Action Creatorのpayloadにresponse.dataを渡してdispatchを実行する
+    dispatch(add(response.data));
+  };
+};
+
+export { add, minus, addAsync };
+export default counter.reducer;
