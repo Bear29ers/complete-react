9918eefab8eb6e4bf5f9103cafc0a29f7ca75050
Yuta Okuma <y.dream.nest@icloud.com>
2022-12-28 08:11:13 +0900

    feat: performance/useTransition

--- react/15_performance/src/060_useTransition/start/Example.js
+++ react/15_performance/src/060_useTransition/start/Example.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+/* [Concurrent Mode] useTransitionでユーザビリティを向上 */
+import { useState, useTransition } from "react";
 
 const generateDummyItem = (num) => {
   return new Array(num).fill(null).map((item, index) => `item ${index}`);
@@ -7,15 +8,21 @@ const generateDummyItem = (num) => {
 const dummyItems = generateDummyItem(10000);
 
 const Example = () => {
+  const [isPending, startTransition] = useTransition();
   const [filterVal, setFilterVal] = useState("");
 
   const changeHandler = (e) => {
-    setFilterVal(e.target.value);
+    // startTransitionの引数に渡したコールバック関数の優先順位が下がる
+    startTransition(() => {
+      setFilterVal(e.target.value);
+    });
   };
 
   return (
     <>
       <input type="text" onChange={changeHandler} />
+      {/* useTransitionの処理が実行中はisPendingがtrueを返す */}
+      {isPending && <div>Loading...</div>}
       <ul>
         {dummyItems
           .filter((item) => {
