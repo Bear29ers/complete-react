0dd4f25a8ef1c758be5bab322af2a5aa5250466a
Yuta Okuma <y.dream.nest@icloud.com>
2022-12-30 02:37:29 +0900

    feat: rest-api/component's dynamic import

--- react/16_rest_api/src/080_react_lazy/start/Example.js
+++ react/16_rest_api/src/080_react_lazy/start/Example.js
@@ -1,13 +1,28 @@
-import { useState } from "react";
-import ComponentA from "./components/ComponentA";
+/* コンポーネントをダイナミックインポート */
+import { lazy, Suspense, useState, startTransition } from "react";
+
+const LazyComponentA = lazy(() => import("./components/ComponentA"));
+const LazyComponentB = lazy(() => import("./components/ComponentB"));
 
 const Example = () => {
-  const [compA, setCompA] = useState(false);
+  const [compA, setCompA] = useState(true);
 
   return (
     <>
-      <button onClick={() => setCompA((prev) => !prev)}>ComponentA</button>
-      {compA && <ComponentA />}
+      <button
+        onClick={() => {
+          // startTransitionメソッドで更新関数を囲うことで、次のコンポーネントが読み込まれるまで、前のコンポーネントを表示してくれる
+          startTransition(() => {
+            setCompA((prev) => !prev);
+          });
+        }}
+      >
+        ComponentA
+      </button>
+      {/* Suspenseコンポーネントでコンポーネントが読み込まれるまでの間のコンテンツを記述できる */}
+      <Suspense fallback={<div>Loading!!!!!</div>}>
+        {compA ? <LazyComponentA /> : <LazyComponentB />}
+      </Suspense>
     </>
   );
 };
