f1832e1b962641a7e4d8253733c72f321fa4e142
Yuta Okuma <y.dream.nest@icloud.com>
2022-12-30 00:35:52 +0900

    feat: rest-api/json-server

--- react/16_rest_api/src/030_json_server/start/Example.js
+++ react/16_rest_api/src/030_json_server/start/Example.js
@@ -1,3 +1,22 @@
+/*
+JSON Serverとは、APIのモックを手軽に作成できるnode.jsのライブラリ。
+APIモックとは、モックアップのことで、開発環境用の簡易APIのこと。
+APIモックを使用することで、開発・テスト用の擬似的なレスポンスを簡単に取得することが可能になる。
+
+JSON Serverをインストールする
+npm i -D json-server
+
+jsonファイルを用意する
+
+オブジェクト形式でパス名をkeyにする
+{
+  "パス名": json形式のデータ
+}
+*/
+
+// json-serverの立ち上げ
+// npx json-server -w src/**/db.json
+
 const Example = () => {
   return (
     <p style={{ textAlign: "center" }}>
