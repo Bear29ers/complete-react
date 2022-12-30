8783bc10405a3a373921b668e85b7b2e5fa9e8a4
Yuta Okuma <y.dream.nest@icloud.com>
2022-12-30 22:33:04 +0900

    feat: nextjs/get path info

--- react/17_nextjs_p1/start/src/pages/07_router/[name]/settings.js
+++ react/17_nextjs_p1/start/src/pages/07_router/[name]/settings.js
@@ -1,3 +1,15 @@
-export default function Setting() {
-  return <h1>[name]/setting.js</h1>;
+import { useRouter } from "next/router";
+
+export default function Setting({ query }) {
+  // パスの情報や画面遷移に使う情報が取得できる
+  const router = useRouter();
+  console.log(router);
+  return <h1>routerから取得：{router.query.name}</h1>;
+}
+
+// SSRの際の関数コンポーネントのpropsを定義する関数
+export async function getServerSideProps({ query }) {
+  return {
+    props: { query },
+  };
 }
