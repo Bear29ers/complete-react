d9dfd0295b167c18eae1982caa542535c5e048bd
Yuta Okuma <y.dream.nest@icloud.com>
2022-12-30 22:53:48 +0900

    feat: nextjs/transition with useRouter

--- react/17_nextjs_p1/start/src/pages/07_router/[name]/settings.js
+++ react/17_nextjs_p1/start/src/pages/07_router/[name]/settings.js
@@ -3,8 +3,27 @@ import { useRouter } from "next/router";
 export default function Setting({ query }) {
   // パスの情報や画面遷移に使う情報が取得できる
   const router = useRouter();
-  console.log(router);
-  return <h1>routerから取得：{router.query.name}</h1>;
+
+  const clickHandler = () => {
+    // 第2引数に設定したURLがブラウザに表示される（実際に遷移するのは第1引数のURL）
+    // router.push("/", "/dummy-url");
+
+    // アクセス履歴が上書きされる（ブラウザバックで戻れなくなる）
+    router.replace("/", "/dummy-url");
+
+    // 一つ前の画面に戻る
+    router.back();
+
+    // 画面を更新する
+    router.reload();
+  };
+
+  return (
+    <>
+      <h1>routerから取得：{router.query.name}</h1>
+      <button onClick={clickHandler}>アクションによる画面遷移</button>
+    </>
+  );
 }
 
 // SSRの際の関数コンポーネントのpropsを定義する関数
