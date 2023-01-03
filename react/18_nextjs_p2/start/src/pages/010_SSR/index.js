b786b1076b13fa6e2d27fd9f3c9b87a757242aa7
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-03 18:37:32 +0900

    feat: nextjs/getServerSideProps

--- react/18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ react/18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -2,17 +2,38 @@ import { useEffect, useState } from "react";
 
 /* Reactは仮想DOMを内部で保持しているので、擬似的にDOMツリーのようなものを作成できるため、
 それを元にHTMLを作成することができる */
-export default function SSR() {
+export default function SSR({ message }) {
   console.log("hello");
+  console.log(message);
 
   // Node.jsはwindowオブジェクトを保持していないので、使用できない → useEffectを使用してwindowオブジェクトを使用する
   // useEffectはNode.js上では実行されず、ブラウザのみで実行される
   useEffect(() => {
     console.log("useEffect");
     window.localStorage.setItem("key", "value");
+    document.cookie = "val=0; path=/;";
   }, []);
 
   const [state, setState] = useState("bye");
   const val = 0;
   return <h3>{state}</h3>;
+}
+
+// Node.js上で実行される関数
+// 引数のcontextには通信の情報が入ってくる
+export async function getServerSideProps(context) {
+  const { cookie } = context.req.headers;
+  console.log("cookie", cookie);
+  console.log("getServerSideProps is executed");
+
+  return {
+    // リダイレクトの設定
+    /* redirect: {
+      destination: "/",
+      // 308のPermanent Redirectと307のTemporary Redirectのどちらにするか設定できる
+      permanent: true,
+    }, */
+    // 関数コンポーネントのpropsの値を定義
+    props: { message: "From Server Side Props" },
+  };
 }
