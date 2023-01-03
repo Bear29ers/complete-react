30fb0c071e60e3ff5a95e9fef80f912e49e383f4
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-03 17:47:17 +0900

    feat: nextjs/behavior of SSR part.2

--- react/18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ react/18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -6,7 +6,9 @@ export default function SSR() {
   console.log("hello");
 
   // Node.jsはwindowオブジェクトを保持していないので、使用できない → useEffectを使用してwindowオブジェクトを使用する
+  // useEffectはNode.js上では実行されず、ブラウザのみで実行される
   useEffect(() => {
+    console.log("useEffect");
     window.localStorage.setItem("key", "value");
   }, []);
 
