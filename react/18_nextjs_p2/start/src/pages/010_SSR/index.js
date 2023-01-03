6fa35ff918148343b4440ce4016096d1da54ff5e
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-03 22:25:52 +0900

    feat: nextjs/generate html

--- react/18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ react/18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 
 /* Reactは仮想DOMを内部で保持しているので、擬似的にDOMツリーのようなものを作成できるため、
 それを元にHTMLを作成することができる */
-export default function SSR({ message }) {
+// export default function SSR({ message }) {
+export default function SSR() {
   console.log("hello");
-  console.log(message);
+  // console.log(message);
 
   // Node.jsはwindowオブジェクトを保持していないので、使用できない → useEffectを使用してwindowオブジェクトを使用する
   // useEffectはNode.js上では実行されず、ブラウザのみで実行される
@@ -21,19 +22,19 @@ export default function SSR({ message }) {
 
 // Node.js上で実行される関数
 // 引数のcontextには通信の情報が入ってくる
-export async function getServerSideProps(context) {
-  const { cookie } = context.req.headers;
-  console.log("cookie", cookie);
-  console.log("getServerSideProps is executed");
-
-  return {
-    // リダイレクトの設定
-    /* redirect: {
-      destination: "/",
-      // 308のPermanent Redirectと307のTemporary Redirectのどちらにするか設定できる
-      permanent: true,
-    }, */
-    // 関数コンポーネントのpropsの値を定義
-    props: { message: "From Server Side Props" },
-  };
-}
+// export async function getServerSideProps(context) {
+//   const { cookie } = context.req.headers;
+//   console.log("cookie", cookie);
+//   console.log("getServerSideProps is executed");
+//
+//   return {
+//     // リダイレクトの設定
+//     /* redirect: {
+//       destination: "/",
+//       // 308のPermanent Redirectと307のTemporary Redirectのどちらにするか設定できる
+//       permanent: true,
+//     }, */
+//     // 関数コンポーネントのpropsの値を定義
+//     props: { message: "From Server Side Props" },
+//   };
+// }
