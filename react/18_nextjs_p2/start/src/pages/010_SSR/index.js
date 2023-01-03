6d4900b11f9d78326e7109964c35b920fe76ac45
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-03 17:41:12 +0900

    feat: nextjs/behavior of SSR part.1

---
+++ react/18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+/* Reactは仮想DOMを内部で保持しているので、擬似的にDOMツリーのようなものを作成できるため、
+それを元にHTMLを作成することができる */
+export default function SSR() {
+  console.log("hello");
+
+  // Node.jsはwindowオブジェクトを保持していないので、使用できない → useEffectを使用してwindowオブジェクトを使用する
+  useEffect(() => {
+    window.localStorage.setItem("key", "value");
+  }, []);
+
+  const [state, setState] = useState("bye");
+  const val = 0;
+  return <h3>{state}</h3>;
+}
