6b667474e65052d599fc7f562a1df9c17211ae67
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-05 08:56:31 +0900

    feat: nextjs/getStaticProps

--- react/18_nextjs_p2/start/src/pages/020_SG/index.js
+++ react/18_nextjs_p2/start/src/pages/020_SG/index.js
@@ -1,4 +1,12 @@
 // Static Site Generation
-export default function IndexPage() {
-  return <h3>SG</h3>;
+export default function IndexPage({ message }) {
+  return <h3>SG:{message}</h3>;
+}
+
+// getStaticProps関数を用いてpropsをページに渡す
+// Node.js上でビルド時に実行される
+export async function getStaticProps() {
+  return {
+    props: { message: "From Static Props" },
+  };
 }
