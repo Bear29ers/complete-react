da330e24c39fb2e1f8f5b65c694e123efc1f9ee9
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-05 09:35:11 +0900

    feat: nextjs/behavior of fallback

--- react/18_nextjs_p2/start/src/pages/020_SG/[id].js
+++ react/18_nextjs_p2/start/src/pages/020_SG/[id].js
@@ -1,9 +1,17 @@
+import { useRouter } from "next/router";
+
 export default function Page({ id }) {
+  const router = useRouter();
+  // ページが存在しない状態だとtrueになる
+  if (router.isFallback) {
+    return <h3>Loading...</h3>;
+  }
   return <h3>このページは{id}です。</h3>;
 }
 
 // getStaticPaths関数を使用して[id]に該当するページを定義する
 export async function getStaticPaths() {
+  console.log("getStaticPaths executed");
   return {
     paths: [
       {
@@ -13,11 +21,17 @@ export async function getStaticPaths() {
         params: { id: "2" },
       },
     ],
-    fallback: false,
+    // paramsに存在しないページへのアクセスがあった場合にNode.jsが自動的にページを生成する
+    // fallback: true,
+    // 別の存在しないページへのアクセス時にisFallbackを無視して遷移する
+    fallback: "blocking",
   };
 }
 
+// contextにparamsの情報が含まれているので、そちらでidを取得する
+// 初回の一度のみ実行される
 export async function getStaticProps({ params }) {
+  console.log("getStaticProps executed");
   return {
     props: {
       id: params.id,
