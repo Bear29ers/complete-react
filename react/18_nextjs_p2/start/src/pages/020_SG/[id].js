843af44f648c0d38ad85bf8a48c5d0400ba37449
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-06 09:09:51 +0900

    feat: nextjs/ISR

--- react/18_nextjs_p2/start/src/pages/020_SG/[id].js
+++ react/18_nextjs_p2/start/src/pages/020_SG/[id].js
@@ -1,12 +1,16 @@
 import { useRouter } from "next/router";
 
-export default function Page({ id }) {
+export default function Page({ id, date }) {
   const router = useRouter();
   // ページが存在しない状態だとtrueになる
   if (router.isFallback) {
     return <h3>Loading...</h3>;
   }
-  return <h3>このページは{id}です。</h3>;
+  return (
+    <h3>
+      このページは{id}です。{date}
+    </h3>
+  );
 }
 
 // getStaticPaths関数を使用して[id]に該当するページを定義する
@@ -32,9 +36,19 @@ export async function getStaticPaths() {
 // 初回の一度のみ実行される
 export async function getStaticProps({ params }) {
   console.log("getStaticProps executed");
+  const date = new Date();
   return {
     props: {
       id: params.id,
+      // 直接Date型のオブジェクトを渡すことができないので、String型に変換する
+      date: date.toJSON(),
     },
+    // 5秒間は静的ファイルを再生成しない
+    revalidate: 5,
+    /*
+    ・SGでは一度生成した静的ファイルは変更されない
+    ・ISRでは設定した秒数ごとに変更がある場合は静的ファイルが更新される
+    ・SSRではリクエストを投げるたびに静的ファイルが更新される
+     */
   };
 }
