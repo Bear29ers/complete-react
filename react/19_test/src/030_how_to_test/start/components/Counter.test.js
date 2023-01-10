093579c3d4247fa8e4bb63de4fc5d8a2b9fcaef0
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-10 20:42:02 +0900

    feat: test/describe

--- react/19_test/src/030_how_to_test/start/components/Counter.test.js
+++ react/19_test/src/030_how_to_test/start/components/Counter.test.js
@@ -1,6 +1,25 @@
-/* POINT テストコードを書く際の注意点
-Arrage:(テストデータ、条件、環境の設定).
-Act:(ロジックの実行、関数の実行).
-Assertion:(実行結果と期待する結果の比較).
+/* テストコードの基本
+Arrage:(テストデータ、条件、環境の設定)
+Act:(ロジックの実行、関数の実行)
+Assertion:(実行結果と期待する結果の比較)
 */
-test('ダミーテスト:削除して記載してください。', () => {});
+
+import { fireEvent, render, screen } from "@testing-library/react";
+import Counter from "./Counter";
+
+test("「カウントアップ」押下で「現在のカウント」が+1される", () => {
+  // Arrange
+  render(<Counter originCount={0} />);
+
+  // 更新前をテストするとより正確になる
+  const spanElBeforeUpdate = screen.getByText("現在のカウント:0");
+  expect(spanElBeforeUpdate).toBeInTheDocument();
+
+  // Act
+  const btn = screen.getByRole("button", { name: "カウントアップ" });
+  fireEvent.click(btn);
+
+  // Assertion
+  const spanEl = screen.getByText("現在のカウント:1");
+  expect(spanEl).toBeInTheDocument();
+});
