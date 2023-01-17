9ef74c647c46ff27cffdab7dae21c69708af1013
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-17 09:03:55 +0900

    feat: typescript/union

--- react/20_typescript/src/040_union_types/start/Example.tsx
+++ react/20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,29 @@
+/* Union型 */
+/* ユニオン型（union type）は、複数の型を組み合わせて「型Tまたは型U」のような、「または」の意味を表すことができる。
+T | U というように、|（パイプ）を用いてユニオン型を表す */
 const Example = () => {
-  // 練習用
+  let strOrNum: string | number = "Hello";
+  strOrNum = 123;
+  console.log(strOrNum);
+
+  // 3つ以上の型も指定可能
+  let strOrNumOrBool: string | number | boolean = "Hello";
+  strOrNumOrBool = true;
+  console.log(strOrNumOrBool);
+
+  // リテラル型も複合可能
+  let helloOrNumOrBool: "Hello" | number | boolean = "Hello";
+  helloOrNumOrBool = false;
+  console.log(helloOrNumOrBool);
+
+  // 型定義
+  type HelloOrNum = "Hello" | number;
+  const hello: HelloOrNum = "Hello";
+  console.log(hello);
+
+  type DayOfWeek = "Monday" | "Tuesday";
+  const day: DayOfWeek = "Monday";
+  console.log(day);
 };
 
 export default Example;
