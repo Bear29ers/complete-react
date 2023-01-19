3136749489ae58ec734db55a6016688b3fd6164a
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-20 08:58:21 +0900

    feat: typescript/function type part.1

--- react/20_typescript/src/080_function_type/start/Example.tsx
+++ react/20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,14 @@
+/* 関数の型定義 */
 const Example = () => {
-  // 練習用
+  // 引数には必ず型を指定する
+  function sum1(x: number, y: number) {
+    return x + y;
+  }
+
+  const result1 = sum1(1, 2);
+  console.log(result1);
+
+  const sum2 = (x: number, y: number) => x + y;
 };
 
 export default Example;
