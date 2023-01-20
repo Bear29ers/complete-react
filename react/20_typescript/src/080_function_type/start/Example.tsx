0bbfbca88f908392a2e63d228f5a04db575c8936
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-20 09:19:03 +0900

    feat: typescript/function type part.2

--- react/20_typescript/src/080_function_type/start/Example.tsx
+++ react/20_typescript/src/080_function_type/start/Example.tsx
@@ -1,14 +1,25 @@
 /* 関数の型定義 */
 const Example = () => {
   // 引数には必ず型を指定する
-  function sum1(x: number, y: number) {
-    return x + y;
+  // void型は戻り値が何もない場合に指定する
+  function sum1(x: number, y: number): void {
+    console.log(y);
+    // return x + y;
   }
 
   const result1 = sum1(1, 2);
   console.log(result1);
 
-  const sum2 = (x: number, y: number) => x + y;
+  // TypeScriptでは戻り値はある程度型推論される
+  const sum2 = (x: number, y: number): number => x + y;
+  const result2 = sum2(10, 20);
+  console.log(result2);
+
+  // 関数の型を定義することも可能
+  type Sum = (x: number, y: number) => number;
+  const sum3: (x: number, y: number) => number = (x, y) => x + y;
+  const result3 = sum3(1, 2);
+  console.log(result3);
 };
 
 export default Example;
