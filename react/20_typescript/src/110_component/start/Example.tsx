198dc434e6e1011d4d455b06b8e9239cd247bbaf
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-22 01:45:20 +0900

    feat: typescript/react component

--- react/20_typescript/src/110_component/start/Example.tsx
+++ react/20_typescript/src/110_component/start/Example.tsx
@@ -1,5 +1,9 @@
-const Example = () => {
-    // 練習
+import Hello from "./Hello";
+
+/* TypeScriptでReactコンポーネントを作成する */
+// React18以降はVFCは非推奨になっているので、FCを使用する
+const Example: React.FC = () => {
+  return <Hello />;
 };
 
 export default Example;
