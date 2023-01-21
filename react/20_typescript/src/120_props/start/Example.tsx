418369196d9df1a9b24a4bdd47b4ff69adb001d3
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-22 02:00:10 +0900

    feat: typescript/props type

--- react/20_typescript/src/120_props/start/Example.tsx
+++ react/20_typescript/src/120_props/start/Example.tsx
@@ -1,5 +1,13 @@
-const Example = () => {
-  // 練習用
+import Hello, { Btn } from "./Hello";
+
+/* Reactコンポーネントでpropsの型定義を行う */
+const Example: React.FC = () => {
+  return (
+    <>
+      <Btn fn={(text) => console.log(`Hello ${text}`)} />
+      <Hello text="TypeScript">Children</Hello>;
+    </>
+  );
 };
 
 export default Example;
