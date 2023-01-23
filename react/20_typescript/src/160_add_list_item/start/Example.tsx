f6a8d04b04bec416000549fc2da7ede47109e1d4
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-23 09:21:05 +0900

    feat: typescript/todo app

--- react/20_typescript/src/160_add_list_item/start/Example.tsx
+++ react/20_typescript/src/160_add_list_item/start/Example.tsx
@@ -1,7 +1,34 @@
+/* 簡易Todoリストの作成 */
 import { useState } from "react";
+import TaskList from "./TaskList";
 
-const Example = () => {
-  
+export type Todo = {
+  id: number;
+  text: string;
+};
+
+const Example: React.FC = () => {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 0, text: "Todo1" },
+    { id: 1, text: "Todo2" },
+  ]);
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setInputText(e.target.value);
+
+  const addTodoItem = () => {
+    setTodos((state) => [...state, { id: todos.length, text: inputText }]);
+    setInputText("");
+  };
+
+  return (
+    <>
+      <input type="text" value={inputText} onChange={changeHandler} />
+      <button onClick={addTodoItem}>追加</button>
+      <TaskList todos={todos} />
+    </>
+  );
 };
 
 export default Example;
